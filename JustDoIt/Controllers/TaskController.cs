0290177e82e9ac0d4dd0fce52fe0c14f1cf34ec2
modem-i4 <vasea.tshuh@gmail.com>
2021-05-31 16:37:49 +0300

    Add project files.

---
+++ JustDoIt/Controllers/TaskController.cs
@@ -0,0 +1,39 @@
+﻿using JustDoIt.Models;
+using JustDoIt.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web.Resource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustDoIt.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private IBaseRepository<TaskModel> Tasks { get; set; }
+        private readonly ILogger<TaskController> _logger;
+
+        // The Web API will only accept tokens 1) for users, and 2) having the "access_as_user" scope for this API
+        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
+
+        public TaskController(ILogger<TaskController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<TaskModel> Get()
+        {
+            //HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+
+            var model = Tasks.GetAll();
+            return model;
+        }
+    }
+}
---
+++ JustDoIt/Models/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using JustDoIt.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustDoIt.Models
+{
+    public class TaskModel : BaseModel
+    {
+        public string Title { get; set; }
+    }
+}
